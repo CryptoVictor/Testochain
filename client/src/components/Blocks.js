2c45ca484fd9025543ecbfec74eb9fd506dc3aae
Victor Garcia <hdjdududydusu@gmail.com>
2022-05-11 06:18:49 -0300

    Update Blocks.js

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2022 Victor Garcia | Special Thanks: David Joseph Katz
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE.txt or https://opensource.org/licenses/MIT
+
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
