9257e9de70081131bfcd82266aaff58a48032a36
Victor Garcia <hdjdududydusu@gmail.com>
2022-05-11 06:19:58 -0300

    Update index.test.js

--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2022 Victor Garcia | Special Thanks: David Joseph Katz
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE.txt or https://opensource.org/licenses/MIT
+
 const Blockchain = require('./index');
 const Block = require('./block');
 const { cryptoHash } = require('../util');
