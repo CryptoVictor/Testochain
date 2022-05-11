fc3f237c2c079f092b5533353dcaf3a08aae58fc
Victor Garcia <hdjdududydusu@gmail.com>
2022-05-11 06:17:49 -0300

    Update history.js

--- client/src/history.js
+++ client/src/history.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2022 Victor Garcia | Special Thanks: David Joseph Katz
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE.txt or https://opensource.org/licenses/MIT
+
 import createBrowserHistory from 'history/createBrowserHistory';
 
 export default createBrowserHistory();
