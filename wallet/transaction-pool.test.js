8c1489e167284c181a5c020bd82e0011072da39d
Victor Garcia <hdjdududydusu@gmail.com>
2022-05-11 06:11:08 -0300

    Update transaction-pool.test.js

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2022 Victor Garcia | Special Thanks: David Joseph Katz
 // Distributed under the MIT software license, see the accompanying
-// file LICENSE or https://opensource.org/licenses/MIT
+// file LICENSE.txt or https://opensource.org/licenses/MIT
 
 const TransactionPool = require('./transaction-pool');
 const Transaction = require('./transaction');
