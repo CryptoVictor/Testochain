57deb48c2148ba2ec29d4cc83f4e01c033a4f458
Victor Garcia <hdjdududydusu@gmail.com>
2022-05-11 06:12:22 -0300

    Update index.js

--- wallet/index.js
+++ wallet/index.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2022 Victor Garcia | Special Thanks: David Joseph Katz
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE.txt or https://opensource.org/licenses/MIT
+
 const Transaction = require('./transaction');
 const { STARTING_BALANCE } = require('../config');
 const { ec, cryptoHash} = require('../util');
