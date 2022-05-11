12ba1a3837bd29c84b74036b6be11db60e66deb9
Victor Garcia <hdjdududydusu@gmail.com>
2022-05-11 06:09:31 -0300

    Update transaction.js

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2022 Victor Garcia | Special Thanks: David Joseph Katz
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or https://opensource.org/licenses/MIT
+
 const uuid = require('uuid/v1');
 const { verifySignature } = require('../util');
 const { REWARD_INPUT, MINING_REWARD }  = require('../config');
